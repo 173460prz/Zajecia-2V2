595d2b793f78c3352751ad6f6591fb5e011948db
173460prz <162919668+173460prz@users.noreply.github.com>
2024-03-13 16:04:14 +0000

    Poprawka

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,19 @@
 
 public class Main {
   public static void main(String[] args) {
+    int i = 100;
+    
     System.out.println("H \n e \n  l \n   l \n    o \n     , \n      w \n       o \n        r \n         l \n          d \n           !");
     System.out.println("H \r\ne \r\nl \r\nl \r\no \r\n \r\nw \r\no \r\nr \r\nl \r\nd \r\n!");
+    
+    while (i != 0)
+      {
+        if (i%4 == 0)
+        {
+          System.out.println(i);
+        }
+        i--;
+      }
     }
 
   // @Test
