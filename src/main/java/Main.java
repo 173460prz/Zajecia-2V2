e8d9e0b83bd874d782caf0e56b95259bc2971940
173460prz <162919668+173460prz@users.noreply.github.com>
2024-03-13 15:47:03 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("H \n e \n  l \n   l \n    o \n     , \n      w \n       o \n        r \n         l \n          d \n           !");
+    System.out.println("H \r\ne \r\nl \r\nl \r\no \r\n \r\nw \r\no \r\nr \r\nl \r\nd \r\n!");
+    }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
